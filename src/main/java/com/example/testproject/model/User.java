c6a56129b31203890e52cc78c9bb4301ec98a7b3
Roma <lutsiv55@gmail.com>
2023-02-03 19:25:51 +0200

    Fixed bugs.

--- src/main/java/com/example/testproject/model/User.java
+++ src/main/java/com/example/testproject/model/User.java
@@ -13,7 +13,6 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
     @Column(name = "firstname")
